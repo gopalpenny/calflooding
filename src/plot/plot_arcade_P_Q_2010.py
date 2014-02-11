56dc8a61bf59f19e5c345a4bcf75cd55a35e660f
Gopal Penny <gopalpenny@gmail.com>
2014-02-10 23:40:32 -0800

    getting closer to finishing arcadePQ plot

--- src/plot/plot_arcade_P_Q_2010.py
+++ src/plot/plot_arcade_P_Q_2010.py
@@ -4,7 +4,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-
 dirformat = '../../data/format/'
 Q_filename = 'arcade_Q_11447360_2008_2013.csv'
 P_filename = 'arcade_P_CIMIS_2008_2013.csv'
@@ -18,9 +17,18 @@ Pdates = pd.to_datetime(arcade_P.dates)
 #arcade_P_in = pd.read_csv(dirformat + P_filename, index_col=0, names=['dates','precip_in'], header=None)
 
 f1 = plt.figure()
-ax1 = f1.add_subplot(2,1,1)
-ax1.plot(Pdates,arcade_P.precip_mm)
-ax1.plot(Qdates,arcade_Q.discharge_cfs)
+ax1a = f1.add_subplot(6,1,1)
+ax1b = ax1a.twinx()
+#ax1a.plot(Pdates,arcade_P.precip_mm)
+#ax1b.plot(Qdates,arcade_Q.discharge_cfs,'g')
+startdate = pd.to_datetime('10/1/2008')
+enddate = pd.to_datetime('10/1/2009')
+prange = (Pdates >= startdate) * (Pdates < enddate) 
+qrange = (Qdates >= startdate) * (Qdates < enddate)
+ax1a.plot(Pdates.values[prange],arcade_P.precip_mm.values[prange])
+ax1b.plot(Qdates.values[qrange],arcade_Q.discharge_cfs.values[qrange],'g')
+
+# plt.show()
 
 out_filename = 'arcade_P_Q'
 out_dir = '../../results/fig/'
